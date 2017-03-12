d502339bd39fead252cae568278b28030c8b31c4
Taehoon Seo <shpongle2634@naver.com>
2017-03-12 13:34:09 +0900

    Fix : ExcelCreator xlsx Update xmlBeans Error

--- JexcelUnit/src/jexcelunit/excel/ExcelCreator.java
+++ JexcelUnit/src/jexcelunit/excel/ExcelCreator.java
@@ -9,6 +9,7 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.poi.ss.usermodel.CellCopyPolicy;
@@ -19,6 +20,7 @@ import org.apache.poi.ss.usermodel.DataValidation;
 import org.apache.poi.ss.usermodel.DataValidationConstraint;
 import org.apache.poi.ss.usermodel.DataValidationHelper;
 import org.apache.poi.ss.usermodel.Drawing;
+import org.apache.poi.ss.usermodel.Name;
 import org.apache.poi.ss.usermodel.RichTextString;
 import org.apache.poi.ss.util.CellRangeAddressList;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -30,7 +32,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTWorkbook;
 
 import jexcelunit.utils.ClassInfo;
 
@@ -54,31 +55,40 @@ public class ExcelCreator{
 	private String projectName= null, rootpath=null;
 	private HashMap<String, ClassInfo> classinfos=null;
 	private File existingExcel= null;
-	
-	
+
+
 
 	public ExcelCreator(String projectName,String rootpath ,HashMap<String, ClassInfo> classinfos){
 		this.projectName=projectName;
 		this.rootpath= rootpath;
 		this.classinfos= classinfos;
-		
+
 		consCount=getMaxParamCount(CONSTRUCTOR,classinfos);
 		metsCount=getMaxParamCount(METHOD,classinfos);
 	}
 
-
 	public void initWorkSheets(XSSFWorkbook workbook){
-		//Init All names.
-		CTWorkbook ctbook=  workbook.getCTWorkbook();
-		ctbook.unsetDefinedNames();
-		ctbook.setDefinedNames(null);
+		//Init All names. 
+		List<XSSFName> names= workbook.getAllNames();
+		String[] rmvName= new String[names.size()];
+		int rm_Nameindex= 0;
+		for(int i =0; i<names.size(); i++){
+			rmvName[rm_Nameindex++]=  names.get(i).getNameName();
+		}
+		for(int j=0; j< rm_Nameindex; j++){
+			if(rmvName[j] !=null){
+				Name name=workbook.getName(rmvName[j]);
+				workbook.removeName(name);
+			}else break;
+		}
+
+
+		String[] rmvSheet= new String[workbook.getNumberOfSheets()];
+		int rm_Sheetindex=0;
 
 		for(int sheet_index=0;sheet_index<workbook.getNumberOfSheets();sheet_index++){
-			boolean isHidden=workbook.isSheetHidden(sheet_index);
-			if(isHidden){//Init hidden Sheets
-					
-					workbook.removeSheetAt(sheet_index);
-
+			if(workbook.isSheetHidden(sheet_index)){//Init hidden Sheets
+				rmvSheet[rm_Sheetindex++]= workbook.getSheetName(sheet_index); //Save Delete Sheet's name
 			}else {
 				XSSFSheet sheet = workbook.getSheetAt(sheet_index);
 				XSSFRow firstRow = sheet.getRow(0);
@@ -120,10 +130,16 @@ public class ExcelCreator{
 							removeColumn(sheet, 2+consCount, -met_change);
 						}
 					}
-
 				}
 				sheet.removeRow(sheet.getRow(0));//remove 첫번째 줄은 삭제할것.
 			}
+		}
+
+		for(int i=0; i<rm_Sheetindex; i++){
+			if(rmvSheet[i] !=null){
+				int where = workbook.getSheetIndex(rmvSheet[i]);
+				workbook.removeSheetAt(where);
+			}else break;
 		}
 
 	}
@@ -152,7 +168,7 @@ public class ExcelCreator{
 	private void removeColumn(XSSFSheet sheet, int from, int offset){
 		XSSFRow currentRow =null;
 		CellCopyPolicy ccp = new CellCopyPolicy();
-
+		
 
 		if(sheet !=null){
 			for(int i=0; i<sheet.getPhysicalNumberOfRows(); i++){
@@ -194,23 +210,23 @@ public class ExcelCreator{
 		}
 		return xlsx;
 	}
-	
-	
-	//create xlsx for Testcases.
-	public void createXlsx() throws IOException{
-		/*
-		 * 테스트 이름, 클래스, 생성자파라미터, 메소드, 메소드파라미터, 리턴, 결과
-		 * 1. 다중 파라미터를 어떻게 ? 파라미터 개수륿 분석해서 칼럼 수 조정할것 DONE
-		 * 2. 모크객체의 이용방법은 어떻게? 모크객체 이름으로 접근하도록을 기본. 모크객체 생성도 엑셀로 지원 가능? DONE
-		 * 3. 테스트방식 : 시나리오 or 독립. => 시트를 슈트단위로 하여 DONE.
-		 * 4. 생성파일 네이밍. => TODO 새창.
-		 * 5. 중복파일 처리. => TODO DATAVALIDATION NAMING
-		 * 	5-1. UPDATE 방식으로 유효성,네임 초기화 및 재설정. 칼럼 조정.
-		 * 6. SUCCESS 처리=> TODO 엑셀에 셋팅.
-		 * 7. 로그처리=>TODO Stack ListView로 제공. 로그파일 생성.
-		 * */
-		//Check .xlxs file is exists
 
+
+	//create xlsx for Testcases.
+	/*
+	 * 테스트 이름, 클래스, 생성자파라미터, 메소드, 메소드파라미터, 리턴, 결과
+	 * 1. 다중 파라미터를 어떻게 ? 파라미터 개수륿 분석해서 칼럼 수 조정할것 DONE
+	 * 2. 모크객체의 이용방법은 어떻게? 모크객체 이름으로 접근하도록을 기본. 모크객체 생성도 엑셀로 지원 가능? DONE
+	 * 3. 테스트방식 : 시나리오 or 독립. => 시트를 슈트단위로 하여 DONE.
+	 * 4. 생성파일 네이밍. => TODO 새창.
+	 * 5. 중복파일 처리. => TODO DATAVALIDATION NAMING
+	 * 	5-1. UPDATE 방식으로 유효성,네임 초기화 및 재설정. 칼럼 조정.
+	 * 6. SUCCESS 처리=> TODO 엑셀에 셋팅.
+	 * 7. 로그처리=>TODO Stack ListView로 제공. 로그파일 생성.
+	 * 8. src 폴더의 경로 =>TODO
+	 * */
+	public void createXlsx() throws IOException{
+		//Check ".xlxs" file is exist.
 		existingExcel=getExistingExcel();
 
 		//Create or Update
@@ -220,7 +236,6 @@ public class ExcelCreator{
 		FileInputStream inputstream=null;
 		try{
 			if(existingExcel!=null){
-				
 				inputstream = new FileInputStream(existingExcel.getCanonicalPath());
 				workbook= new XSSFWorkbook(inputstream);
 				initWorkSheets(workbook);
@@ -230,77 +245,57 @@ public class ExcelCreator{
 				xssfSheet= workbook.createSheet("TestSuite 1");	
 			}
 			//make hidden sheet
-
 			hiddensheet(workbook,classinfos);
 
-			row=xssfSheet.createRow(0);//번줄은 info
+			for(int sheet_index=0; sheet_index<workbook.getNumberOfSheets(); sheet_index++){
+				if( !workbook.isSheetHidden(sheet_index) && !workbook.isSheetVeryHidden(sheet_index)){
+					xssfSheet=workbook.getSheetAt(sheet_index);
+					row=xssfSheet.createRow(0);//info
 
-			//칼럼의 갯수는 생성자 혹은 메소드 파라미터에 의해 가변적으로 정할것.
-			//칼럼에 Data Validation을 지정.
+					int cellvalindex=0;
+					int totalCellCount= TESTDATASET.length + consCount + metsCount-2;
+					for(int i =0; i<totalCellCount; i++){
+						String val=TESTDATASET[cellvalindex]; //Column List.
+						if(val.equals("Constructor Param")){					
+							//Set Param Validation Type
+							for(int k=0; k<consCount; k++){
+								cell=row.createCell(i+k);
+								xssfSheet.setColumnWidth(i+k, 4500);
+								cell.setCellValue(val+(k+1));
 
-			int cellvalindex=0;
-			int totalCellCount= TESTDATASET.length + consCount + metsCount-2;
-			for(int i =0; i<totalCellCount; i++){
-				String val=TESTDATASET[cellvalindex];
+							}
+							i+=consCount-1;
+							cellvalindex++;
+						}
+						else if(val.equals("TestMethod")){
+							xssfSheet.setColumnWidth(i, 3000);
+							setValidation("INDIRECT(LEFT($B2,FIND(\"(\",$B2)-1))", xssfSheet, i);
+							cell=row.createCell(i);
+							cell.setCellValue(val);
+							cellvalindex++;
+						}
+						else if(val.equals("Method Param")){
 
-				if(val.equals("Constructor Param")){					
-					//Set Param Validation Type
-					/*
-					 * 03 - 08 이슈 정리 : 
-					 * 1. 로그 관리 : 로그 파일 생성 로그에 표시할 정보를 정해야.. =>TODO
-					 * 1-1. success 설정. @after 메소드로 처리?.
-
-					 * 2. 독립/시나리오 테스팅 => sheet를 suite 단위로  => DONE
-					 * 2-1 시트를 생성하고 싶을때 => 복사해야하나. ㅇㅇ
-					 * 2-2 sheet를 실행할 경우.한 sheet당 TestSuite클래스를 suite단위로 추가할 수 있는지. ㅇㅇ
-
-					 * 3. 덮어쓰기 시 data validation 재설정. 셀 변경. => TODO on 3.11
-					 * 3-1 메소드 파라미터가 늘어 셀을 추가해야할 경우
-					 * 3-2 메소드 파라미터가 줄어 셀을 삭제해야할 경우.
-
-					 * 4. 프로젝트 src 폴더가 다른경우가 존재. ex) 웹. =>TODO
-					 * => 프로퍼티 설정으로 src폴더를 지정해야할듯
-					 * */
-					for(int k=0; k<consCount; k++){
-						cell=row.createCell(i+k);
-						xssfSheet.setColumnWidth(i+k, 4500);
-						cell.setCellValue(val+(k+1));
-
+							for(int k=0; k<metsCount; k++){
+								cell=row.createCell(i+k);
+								xssfSheet.setColumnWidth(i+k, 4000);
+								cell.setCellValue(val+(k+1));
+							}
+							i+=metsCount-1;
+							cellvalindex++;
+						}
+						else{
+							xssfSheet.setColumnWidth(i, 3000);
+							cell=row.createCell(i);
+							cell.setCellValue(val);
+							cellvalindex++;
+						}
 					}
-					i+=consCount-1;
-					cellvalindex++;
+					setValidation("Class", xssfSheet, 1);
 				}
-				else if(val.equals("TestMethod")){
-					xssfSheet.setColumnWidth(i, 3000);
-					setValidation("INDIRECT(LEFT($B2,FIND(\"(\",$B2)-1))", xssfSheet, i);
-					cell=row.createCell(i);
-					cell.setCellValue(val);
-					cellvalindex++;
-				}
-
-				else if(val.equals("Method Param")){
-
-					for(int k=0; k<metsCount; k++){
-						cell=row.createCell(i+k);
-						xssfSheet.setColumnWidth(i+k, 4000);
-						cell.setCellValue(val+(k+1));
-					}
-					i+=metsCount-1;
-					cellvalindex++;
-				}
-				else{
-					xssfSheet.setColumnWidth(i, 3000);
-					cell=row.createCell(i);
-					cell.setCellValue(val);
-					cellvalindex++;
-				}
-
 			}
-
-			setValidation("Class", xssfSheet, 1);
 			//save xlsx
 			fileoutputstream=new FileOutputStream(rootpath+"/"+ projectName+".xlsx");
-			//파일을 쓴다
 			workbook.write(fileoutputstream);
 
 			System.out.println("Created");
@@ -308,7 +303,6 @@ public class ExcelCreator{
 			e.printStackTrace();
 		}finally{
 			//필수로 닫아주어야함
-
 			if(workbook!=null) workbook.close();
 			if( fileoutputstream!=null)
 				fileoutputstream.close();
@@ -317,8 +311,6 @@ public class ExcelCreator{
 				fileoutputstream.close();
 			if(inputstream !=null) inputstream.close();
 		}
-
-
 	}
 
 	/*
@@ -341,6 +333,7 @@ public class ExcelCreator{
 		XSSFSheet method_param_sheet = workbook.createSheet("MethodParamhidden");
 		//클래스리스트 및 메소드 이름 유효성-완료
 		XSSFSheet class_method_sheet = workbook.createSheet("ClassMethodhidden");
+
 		Set<String> keys = classinfos.keySet();
 		ClassInfo info =null;
 		XSSFRow clz_met_firstrow=class_method_sheet.createRow(0);
@@ -510,9 +503,9 @@ public class ExcelCreator{
 		System.out.println("total : " + cons_total);
 
 		//Set hidden Sheet if true=  hidden.
-		workbook.setSheetHidden(1, true);
-		workbook.setSheetHidden(2, true);
-		workbook.setSheetHidden(3, true);
+		if(!workbook.isSheetHidden(1)) workbook.setSheetHidden(1, true);
+		if(!workbook.isSheetHidden(2)) workbook.setSheetHidden(2, true);
+		if(!workbook.isSheetHidden(3)) workbook.setSheetHidden(3, true);
 	}
 
 
@@ -558,11 +551,7 @@ public class ExcelCreator{
 						max = met.getParameterCount();
 				}
 			}
-
 		}
 		return max;
 	}
-
-
-
 }
